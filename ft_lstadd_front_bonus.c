b5e98fcc08ef62f1cd96c3b9ee83227e391c4adc
MarouanZelouani <marouanzelouani01@gmail.com>
2023-11-18 21:15:10 +0100

    last changes in bonus part

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzelouan <mzelouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 23:22:59 by mzelouan          #+#    #+#             */
-/*   Updated: 2023/11/17 04:01:58 by mzelouan         ###   ########.fr       */
+/*   Updated: 2023/11/18 21:09:47 by mzelouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (lst && new)
+	if (lst)
 	{
-		new->next = *lst;
+		if (*lst)
+			new->next = *lst;
 		*lst = new;
 	}
 }
