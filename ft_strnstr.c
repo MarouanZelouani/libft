d9a473cba4cb2a07752ea70d21b1f368503282a9
MarouanZelouani <marouanzelouani01@gmail.com>
2023-11-19 22:40:47 +0100

    done for sure

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mzelouan <mzelouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 23:26:22 by mzelouan          #+#    #+#             */
-/*   Updated: 2023/11/17 22:42:18 by mzelouan         ###   ########.fr       */
+/*   Updated: 2023/11/19 22:39:56 by mzelouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ char	*ft_strnstr(const char *big, const char *little, size_t n)
 
 	i = 0;
 	j = 0;
-	if (!big && !n)
-		return (NULL);
 	if (!*little)
 		return ((char *)big);
+	if (!big && !n)
+		return (NULL);
 	while (big[i] != '\0' && i < n)
 	{
 		j = 0;
@@ -33,4 +33,22 @@ char	*ft_strnstr(const char *big, const char *little, size_t n)
 		i++;
 	}
 	return (NULL);
+}
+
+int main ()
+{
+	//strnstr(NULL, "marouan", 0); no seg
+	//strnstr(NULL, "marouan", 2); seg
+	//strnstr(NULL, NULL, 0); seg
+	//strnstr(NULL, "231", 2); seg
+	//strnstr("mar", NULL, 0); seg
+	//strnstr("marou", NULL, 2);
+	
+	// ft_strnstr(NULL, "marouan", 0); // no seg
+	// ft_strnstr(NULL, "marouan", 2);
+	// ft_strnstr(NULL, NULL, 0);
+	// ft_strnstr(NULL, "231", 2);
+	// ft_strnstr("mar", NULL, 0);
+	// ft_strnstr("marou", NULL, 2)
+	//ft_strnstr("marouan", NULL, 7);
 }
