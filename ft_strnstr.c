97b6cf832f7c977d1a52fdbd941ee53f4559d996
MarouanZelouani <marouanzelouani01@gmail.com>
2023-11-19 22:43:05 +0100

    libft final

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mzelouan <mzelouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 23:26:22 by mzelouan          #+#    #+#             */
-/*   Updated: 2023/11/19 22:39:56 by mzelouan         ###   ########.fr       */
+/*   Updated: 2023/11/19 22:41:24 by mzelouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,4 @@ char	*ft_strnstr(const char *big, const char *little, size_t n)
 		i++;
 	}
 	return (NULL);
-}
-
-int main ()
-{
-	//strnstr(NULL, "marouan", 0); no seg
-	//strnstr(NULL, "marouan", 2); seg
-	//strnstr(NULL, NULL, 0); seg
-	//strnstr(NULL, "231", 2); seg
-	//strnstr("mar", NULL, 0); seg
-	//strnstr("marou", NULL, 2);
-	
-	// ft_strnstr(NULL, "marouan", 0); // no seg
-	// ft_strnstr(NULL, "marouan", 2);
-	// ft_strnstr(NULL, NULL, 0);
-	// ft_strnstr(NULL, "231", 2);
-	// ft_strnstr("mar", NULL, 0);
-	// ft_strnstr("marou", NULL, 2)
-	//ft_strnstr("marouan", NULL, 7);
 }
